a2c0846b7c06e6312ab83a9ee210c054aa9b91b0
Nicanor Perera <nicanorperera@gmail.com>
2016-10-18 23:50:25 -0300

    Fifth test pases.

--- lib/acordes/transposer.ex
+++ lib/acordes/transposer.ex
@@ -1,7 +1,33 @@
 defmodule Acordes.Transposer do
 
-  def call("do", 1), do: "do#"
-  def call("do", 2), do: "re"
-  def call("re", _semitones), do: "mi"
+  def call(note, semitones) do
+    note
+    |> pos
+    |> augment(semitones)
+    |> get_note
+  end
+
+  defp pos(  "Do"), do:  0
+  defp pos( "Do#"), do:  1
+  defp pos(  "Re"), do:  2
+  defp pos( "Re#"), do:  3
+  defp pos(  "Mi"), do:  4
+  defp pos(  "Fa"), do:  5
+  defp pos( "Fa#"), do:  6
+  defp pos( "Sol"), do:  7
+  defp pos("Sol#"), do:  8
+  defp pos(  "La"), do:  9
+  defp pos( "La#"), do: 10
+  defp pos(  "Si"), do: 11
+
+  defp augment(position, semitones) do
+    rem (position + semitones), 12
+  end
+
+  defp get_note(position) do
+    {:ok, note} = ["Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"]
+    |> Enum.fetch(position)
+    note
+  end
 
 end
--- test/acordes_test.exs
+++ test/acordes_test.exs
@@ -4,50 +4,33 @@ defmodule AcordesTest do
   alias Acordes.Transposer
 
   test "'do' augmented 2 semitones becomes 're'" do
-    assert Transposer.call("do", 2) == "re"
+    assert Transposer.call("Do", 2) == "Re"
   end
 
   test "'re' augmented 2 semitones becomes 'mi'" do
-    assert Transposer.call("re", 2) == "mi"
+    assert Transposer.call("Re", 2) == "Mi"
   end
 
   test "'do' augmented 1 semitone becomes 'do#'" do
-    assert Transposer.call("do", 1) == "do#"  
+    assert Transposer.call("Do", 1) == "Do#"
   end
 
-#   it "transposes all 12 mayor notes one tone higher" do
-#      transposer = ChordTransposer::Transposer.new(2)
-#      original_chords = ['Do', 'Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si']
-#      expected_chords = ['Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si', 'Do', 'Do#']
-#      (0..11).each do |index|
-#        expect(transposer.call(original_chords[index])).to eq(expected_chords[index])
-#      end
-#    end
-#
-#    it "transposes all 12 mayor notes three semitones higher" do
-#      transposer = ChordTransposer::Transposer.new(3)
-#      original_chords = ['Do', 'Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si']
-#      expected_chords = ['Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si', 'Do', 'Do#', 'Re']
-#      (0..11).each do |index|
-#        expect(transposer.call(original_chords[index])).to eq(expected_chords[index])
-#      end
-#    end
-#
-#    it "transposes all 12 mayor notes twelve semitones higher (and leaves them exactly the same)" do
-#      transposer = ChordTransposer::Transposer.new(12)
-#      original_chords = ['Do', 'Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si']
-#      expected_chords = ['Do', 'Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si']
-#      (0..11).each do |index|
-#        expect(transposer.call(original_chords[index])).to eq(expected_chords[index])
-#      end
-#    end
-#
-#    it "transposes all 12 mayor notes 5 semitones lower" do
-#      transposer = ChordTransposer::Transposer.new(-5)
-#      original_chords = ['Do', 'Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#', 'Sol', 'Sol#', 'La', 'La#', 'Si']
-#      expected_chords = ['Sol', 'Sol#', 'La', 'La#', 'Si', 'Do', 'Do#', 'Re', 'Re#', 'Mi', 'Fa', 'Fa#']
-#      (0..11).each do |index|
-#        expect(transposer.call(original_chords[index])).to eq(expected_chords[index])
-#      end
-#    end
+  test "can augment all 12 mayor notes one tone" do
+    original_chords = ["Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"]
+    expected_chords = ["Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si", "Do", "Do#"]
+    assert expected_chords == Enum.map(original_chords, fn (note) -> Transposer.call(note, 2) end)
+  end
+
+  test "can diminish all 12 mayor notes one tone" do
+    original_chords = ["Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si", "Do", "Do#"]
+    expected_chords = ["Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"]
+    assert expected_chords == Enum.map(original_chords, fn (note) -> Transposer.call(note, -2) end)
+  end
+
+  # test "can augment all 12 minor notes one tone" do
+  #   original_chords = ["Dom", "Do#m", "Rem", "Re#m", "Mim", "Fam", "Fa#m", "Solm", "Sol#m", "Lam", "La#m", "Sim"]
+  #   expected_chords = ["Rem", "Re#m", "Mim", "Fam", "Fa#m", "Solm", "Sol#m", "Lam", "La#m", "Sim", "Dom", "Do#m"]
+  #   assert expected_chords == Enum.map(original_chords, fn (note) -> Transposer.call(note, 2) end)
+  # end
+
 end
