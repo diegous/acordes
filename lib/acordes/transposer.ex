1603c3808db110393beba8edf72a26442fd58078
Nicanor Perera <nicanorperera@gmail.com>
2016-10-21 00:50:57 -0300

    Passing all tests.

--- lib/acordes/transposer.ex
+++ lib/acordes/transposer.ex
@@ -1,27 +1,29 @@
 defmodule Acordes.Transposer do
 
-  @notes    ["Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"]
+  @regex  ~r/(?<tone>Do#|Do|Reb|Re#|Re|Mib|Mi|Fa#|Fa|Solb|Sol#|Sol|Lab|La#|La|Sib|Si|C#|C|Db|D#|D|Eb|E|F#|F|Gb|G#|G|Ab|A#|A|Bb|B)(?<mode>m7|m|13|11|9|7|6|)/
+
+  @latin    ["Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"]
   @american ["C" , "C#" , "D" , "D#" , "E" , "F" , "F#" , "G"  , "G#"  , "A" , "A#" , "B"]
 
-  @notes_positions (Enum.with_index(@notes) ++ Enum.with_index(@american)) |> Enum.into(%{})
+  @notes_positions (Enum.with_index(@latin) ++ Enum.with_index(@american)) |> Enum.into(%{})
 
   def call(note, semitones) do
-    if Enum.member?(@american, note) do
-      call(note, semitones, :american)
-    else
+    if String.starts_with?(note, @latin) do
       call(note, semitones, :latin)
+    else
+      call(note, semitones, :american)
     end
   end
 
   def call(note, semitones, notation) do
-    if Enum.member?((@notes ++ @american), note) do
-      note
-      |> position
-      |> augment(semitones)
-      |> get_note(notation)
-    else
-      :not_a_note
-    end
+    %{"tone" => tone, "mode" => mode} =
+      @regex |> Regex.named_captures(note)
+
+    tone
+    |> position
+    |> augment(semitones)
+    |> get_note(notation)
+    |> Kernel.<>(mode)
   end
 
   defp position(note) do
@@ -33,7 +35,7 @@ defmodule Acordes.Transposer do
   end
 
   defp get_note(position, :latin) do    
-    @notes |> Enum.fetch!(position)
+    @latin |> Enum.fetch!(position)
   end
 
   defp get_note(position, :american) do    
--- test/acordes_test.exs
+++ test/acordes_test.exs
@@ -27,10 +27,6 @@ defmodule AcordesTest do
     assert expected_chords == Enum.map(original_chords, fn (note) -> Transposer.call(note, -2) end)
   end
 
-  test "only notes can be transposed. Anything else is not_a_note" do
-    assert Transposer.call("Not a note", 1) == :not_a_note
-  end
-
   test "can return american notation" do
     assert Transposer.call("La", 0, :american) == "A"
   end
@@ -39,10 +35,22 @@ defmodule AcordesTest do
     assert Transposer.call("A", 2) == "B"
   end
 
-  # test "can augment all 12 minor notes one tone" do
-  #   original_chords = ["Dom", "Do#m", "Rem", "Re#m", "Mim", "Fam", "Fa#m", "Solm", "Sol#m", "Lam", "La#m", "Sim"]
-  #   expected_chords = ["Rem", "Re#m", "Mim", "Fam", "Fa#m", "Solm", "Sol#m", "Lam", "La#m", "Sim", "Dom", "Do#m"]
-  #   assert expected_chords == Enum.map(original_chords, fn (note) -> Transposer.call(note, 2) end)
-  # end
+  test "can augment all 12 minor notes one tone" do
+    original_chords = ["Dom", "Do#m", "Rem", "Re#m", "Mim", "Fam", "Fa#m", "Solm", "Sol#m", "Lam", "La#m", "Sim"]
+    expected_chords = ["Rem", "Re#m", "Mim", "Fam", "Fa#m", "Solm", "Sol#m", "Lam", "La#m", "Sim", "Dom", "Do#m"]
+    assert expected_chords == Enum.map(original_chords, fn (note) -> Transposer.call(note, 2) end)
+  end
+
+  test "can augment all 12 7th notes one tone" do
+    original_chords = ["Do7", "Do#7", "Re7", "Re#7", "Mi7", "Fa7", "Fa#7", "Sol7", "Sol#7", "La7", "La#7", "Si7"]
+    expected_chords = ["Re7", "Re#7", "Mi7", "Fa7", "Fa#7", "Sol7", "Sol#7", "La7", "La#7", "Si7", "Do7", "Do#7"]
+    assert expected_chords == Enum.map(original_chords, fn (note) -> Transposer.call(note, 2) end)
+  end
+
+  test "can augment all 12 minor 7th notes one tone" do
+    original_chords = ["Dom7", "Do#m7", "Rem7", "Re#m7", "Mim7", "Fam7", "Fa#m7", "Solm7", "Sol#m7", "Lam7", "La#m7", "Sim7"]
+    expected_chords = ["Rem7", "Re#m7", "Mim7", "Fam7", "Fa#m7", "Solm7", "Sol#m7", "Lam7", "La#m7", "Sim7", "Dom7", "Do#m7"]
+    assert expected_chords == Enum.map(original_chords, fn (note) -> Transposer.call(note, 2) end)
+  end
 
 end
